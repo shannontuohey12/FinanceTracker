677b4dcc99312f11c11938899fa5c78ebb7fd887
shannontuohey12 <shannontuohey12@gmail.com>
2026-07-03 22:13:29 -0400

    refactor: update React version and restructure components
    - Downgraded React and ReactDOM to version 18.3.1 in package.json
    - Refactored App.js to implement React Router with nested routes
    - Moved Navbar, Register, Login, and Profile components to a new pages directory
    - Deleted old Login, Navbar, and Register components and created new ones in the pages directory
    - Updated Navbar to use Link components for navigation
    - Added Profile component with a simple header

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Register from './components/Register';
-import Login from './components/Login';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Navbar from './components/pages/Navbar';
+import Register from './components/pages/Register';
+import Login from './components/pages/Login';
+import Profile from './components/pages/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Register />
       <Login />
-    </div>
+      <Profile />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path= '/' element={<Navbar />} >
+            <Route path="Login" element={<Login/>}/>
+            <Route path="Register" element={<Register/>}/>
+          </Route>
+        </Routes>
+    </BrowserRouter>
+    
+      </div>
   );
 }
 
